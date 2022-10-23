e8a9aa8712c59019a672426bcd4d7f1927d210b5
Gar2d2 <pghost286@gmail.com>
2022-10-23 15:01:25 +0200

    *indicator arrow added

--- Zaliczenie/Assets/Scripts/PlayerMove.cs
+++ Zaliczenie/Assets/Scripts/PlayerMove.cs
@@ -15,27 +15,38 @@ public class PlayerMove : MonoBehaviour
     private bool m_bIsJumping = false;
     public float m_playerSpeed = 1.0f;
 
+    private GameObject m_indicatorArrow;
+
     public Gamepad m_playerGamepad { get; set; }
     public Mouse m_playerMouse { get; set; }
     public Keyboard m_playerKeyboard { get; set; }
     private Animator m_Animator;
-    private InputAction m_accelerateAction = new InputAction();
+
+    private InputAction m_walkingAction = new InputAction("Walking");
+    private InputAction m_aimingAction = new InputAction("StartAiming");
+
     private Rigidbody2D m_rigidbody;
     HashSet<Action> m_methodsOnUpdate = new HashSet<Action>();
     // Start is called before the first frame update
     void Start()
     {
-        if(m_playerGamepad != null)
+        m_indicatorArrow = this.transform.Find("IndicatorArrow").gameObject;
+
+        RenderArrow(false);
+
+        if (m_playerGamepad != null)
         {
             MapGamepad();
         }
-        if(m_playerMouse != null && m_playerKeyboard != null)
+        if (m_playerMouse != null && m_playerKeyboard != null)
         {
             MapMouseAndKeyboard();
         }
-        m_accelerateAction.performed += Walk;
-        m_accelerateAction.canceled += EndWalk;
-        m_accelerateAction.Enable();
+        m_walkingAction.performed += Walk;
+        m_walkingAction.canceled += EndWalk;
+        m_walkingAction.Enable();
+
+        m_aimingAction.Enable();
         m_rigidbody = GetComponent<Rigidbody2D>();
         m_Animator = GetComponent<Animator>();
     }
@@ -46,8 +57,11 @@ public class PlayerMove : MonoBehaviour
         var jump = new InputAction(binding: gamepadName + m_playerGamepad.aButton.name);
         jump.performed += _ => Jump();
         jump.Enable();
-        m_accelerateAction.AddBinding(m_playerGamepad.leftStick);
-       
+        m_walkingAction.AddBinding(m_playerGamepad.leftStick);
+        m_aimingAction.AddBinding(m_playerGamepad.rightTrigger);
+        m_aimingAction.performed += StartAiming;
+        m_aimingAction.canceled += StopAiming;
+
     }
     private void MapMouseAndKeyboard()
     {
@@ -57,11 +71,18 @@ public class PlayerMove : MonoBehaviour
         jump.performed += _ => Jump();
         jump.Enable();
 
-        m_accelerateAction.AddCompositeBinding("2DVector") // Or "Dpad"
+        m_walkingAction.AddCompositeBinding("2DVector") // Or "Dpad"
                 .With("Up", "<Keyboard>/w")
                 .With("Down", "<Keyboard>/s")
                 .With("Left", "<Keyboard>/a")
                 .With("Right", "<Keyboard>/d");
+
+
+        jump.performed += _ => Jump();
+        jump.Enable();
+        m_aimingAction.AddBinding(m_playerMouse.leftButton);
+        m_aimingAction.performed += StartAiming;
+        m_aimingAction.canceled += StopAiming;
     }
     void Jump()
     {
@@ -82,7 +103,8 @@ public class PlayerMove : MonoBehaviour
         moveVector.x *= m_playerSpeed;
         moveVector.y = m_rigidbody.velocity.y;
         m_rigidbody.velocity = moveVector;
-        
+
+
     }
     void EndWalk(CallbackContext ctx)
     {
@@ -90,20 +112,40 @@ public class PlayerMove : MonoBehaviour
         m_methodsOnUpdate.Remove(OnWalking);
     }
 
-        // Update is called once per frame
-    void Update()
-    {
 
-    }
-    void FixedUpdate()
+    private void StartAiming(CallbackContext ctx)
     {
-        m_Animator.SetFloat("playerSpeed", m_rigidbody.velocity.x);
-        foreach (Action a in m_methodsOnUpdate)
+        RenderArrow(true);
+        if(ctx.control.device is Mouse)
         {
-            a.Invoke();
+            m_methodsOnUpdate.Add(IndicatorFollowMouse);
+        }
+        else if(ctx.control.device is Gamepad)
+        {
+            m_methodsOnUpdate.Add(IndicatorFollowRightStick);
         }
     }
 
+    private void RenderArrow(bool bRender)
+    {
+        foreach (var rend in m_indicatorArrow.GetComponentsInChildren(typeof(Renderer)))
+        {
+            ((Renderer)rend).enabled = bRender;
+        }
+    }
+
+    private void StopAiming(CallbackContext ctx)
+    {
+        RenderArrow(false);
+        if (ctx.control.device is Mouse)
+        {
+            m_methodsOnUpdate.Remove(IndicatorFollowMouse);
+        }
+        else if (ctx.control.device is Gamepad)
+        {
+            m_methodsOnUpdate.Remove(IndicatorFollowRightStick);
+        }
+    }
     private void StopWalking()
     {
         Vector2 moveInput = m_rigidbody.velocity;
@@ -126,5 +168,44 @@ public class PlayerMove : MonoBehaviour
         m_Animator.SetBool("bIsJumping", false);
         m_bIsJumping = false;
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        m_Animator.SetFloat("playerSpeed", m_rigidbody.velocity.x);
+        foreach (Action a in m_methodsOnUpdate)
+        {
+            a.Invoke();
+        }
+
+    }
+
+    private void IndicatorFollowMouse()
+    {
+        var mouseScreenPosition = m_playerMouse.position.ReadValue();
+        mouseScreenPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+
+        Vector2 dir = new Vector2(
+            mouseScreenPosition.x - m_indicatorArrow.transform.position.x,
+            mouseScreenPosition.y - m_indicatorArrow.transform.position.y
+            );
+
+        m_indicatorArrow.transform.up = dir;
+    }
+    private void IndicatorFollowRightStick()
+    {
+        var gamepadVal = m_playerGamepad.rightStick.ReadValue();
+        if(gamepadVal.magnitude < 0.1)
+        {
+            return;
+        }
+        gamepadVal.Normalize();
+        m_indicatorArrow.transform.up = gamepadVal;
     }
 }
